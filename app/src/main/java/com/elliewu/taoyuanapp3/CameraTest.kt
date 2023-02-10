c4f16028120f254a0c8386868f3e7fff7a6050eb
mikebimfm <mikebimfm@gmail.com>
2023-02-10 17:18:17 +0800

    Gallery OK

--- app/src/main/java/com/elliewu/taoyuanapp3/CameraTest.kt
+++ app/src/main/java/com/elliewu/taoyuanapp3/CameraTest.kt
@@ -3,6 +3,9 @@ package com.elliewu.taoyuanapp3
 import android.Manifest
 import android.content.pm.PackageManager
 import android.net.Uri
+import android.util.Base64.NO_WRAP
+import android.util.Base64.encodeToString
+import android.widget.Gallery
 import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
@@ -28,7 +31,9 @@ import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import coil.compose.ImagePainter.State.Empty.painter
 import coil.compose.rememberImagePainter
+import java.io.File
 import java.util.*
 
 @Preview
@@ -74,54 +79,102 @@ fun CameraTest(navController: NavHostController = rememberNavController()){
                 color = Color(255, 255, 255),
             )
         }
-
-        val context = LocalContext.current
-        val file = context.createImageFile()
-        val uri = FileProvider.getUriForFile(
-            Objects.requireNonNull(context),
-            BuildConfig.APPLICATION_ID + ".provider", file
-        )
-
-        var capturedImageUri by remember {
-            mutableStateOf<Uri>(Uri.EMPTY)
-        }
-
-        val cameraLauncher =
-            rememberLauncherForActivityResult(ActivityResultContracts.TakePicture()) {
-                capturedImageUri = uri
-            }
-
-        val permissionLauncher = rememberLauncherForActivityResult(
-            ActivityResultContracts.RequestPermission()
-        ) {
-            if (it) {
-                Toast.makeText(context, "Permission Granted", Toast.LENGTH_SHORT).show()
-                cameraLauncher.launch(uri)
-            } else {
-                Toast.makeText(context, "Permission Denied", Toast.LENGTH_SHORT).show()
-            }
-        }
-
-        Button(onClick = {
-            val permissionCheckResult =
-                ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA)
-            if (permissionCheckResult == PackageManager.PERMISSION_GRANTED) {
-                cameraLauncher.launch(uri)
-            } else {
-                // Request a permission
-                permissionLauncher.launch(Manifest.permission.CAMERA)
-            }
-        }) {
-            Text(text = "拍照")
-        }
-        if (capturedImageUri.path?.isNotEmpty() == true) {
-            Image(
-                modifier = Modifier
-                    .padding(16.dp, 8.dp),
-                painter = rememberImagePainter(capturedImageUri),
-                contentDescription = null
-            )
-        }
+        Camera()
+        //ImagePicker()
     }
 }
+
+@Composable
+fun Camera() {
+    //相機功能
+    val context = LocalContext.current
+    val file = context.createImageFile()
+    val uri = FileProvider.getUriForFile(
+        Objects.requireNonNull(context),
+        BuildConfig.APPLICATION_ID + ".provider", file
+    )
+
+    var capturedImageUri by remember {
+        mutableStateOf<Uri>(Uri.EMPTY)
+    }
+
+    val cameraLauncher =
+        rememberLauncherForActivityResult(ActivityResultContracts.TakePicture()) {
+            capturedImageUri = uri
+        }
+
+    val galleryLauncher =
+        rememberLauncherForActivityResult(ActivityResultContracts.GetContent()) {
+                uri ->
+            if (uri != null) {
+                capturedImageUri = uri
+            }
+        }
+
+    val permissionLauncher = rememberLauncherForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) {
+        if (it) {
+            Toast.makeText(context, "Permission Granted", Toast.LENGTH_SHORT).show()
+            cameraLauncher.launch(uri)
+        } else {
+            Toast.makeText(context, "Permission Denied", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    val imagepersissionLauncher = rememberLauncherForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ){
+        if(it) {
+            Toast.makeText(context, "Permission Granted", Toast.LENGTH_SHORT).show()
+            galleryLauncher.launch("image/*")
+        }else {
+            Toast.makeText(context, "Permission Denied", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+    Row(
+    ) {
+        Button(
+            modifier = Modifier.padding(horizontal = 8.dp),
+            onClick = {
+                val permissionCheckResult =
+                    ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA)
+                if (permissionCheckResult == PackageManager.PERMISSION_GRANTED) {
+                    cameraLauncher.launch(uri)
+                } else {
+                    // Request a permission
+                    permissionLauncher.launch(Manifest.permission.CAMERA)
+                }
+            }) {
+            Text(text = "拍照")
+        }
+        //相簿功能
+        Button(
+            modifier = Modifier.padding(horizontal = 8.dp),
+            onClick = {
+                val permissionCheckResult =
+                    ContextCompat.checkSelfPermission(context, Manifest.permission.READ_MEDIA_IMAGES)
+                if (permissionCheckResult == PackageManager.PERMISSION_GRANTED) {
+                    galleryLauncher.launch("image/*")
+                }else {
+                    imagepersissionLauncher.launch(Manifest.permission.READ_MEDIA_IMAGES)
+                }
+            }) {
+            Text(text = "相簿")
+        }
+    }
+
+    if (capturedImageUri.path?.isNotEmpty() == true) {
+        Image(
+            modifier = Modifier
+                .padding(16.dp, 8.dp)
+                .background(Color.Yellow),
+            painter = rememberImagePainter(capturedImageUri),
+            contentDescription = null
+        )
+    }
+}
+
 
