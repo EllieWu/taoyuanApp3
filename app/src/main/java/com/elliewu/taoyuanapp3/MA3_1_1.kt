712cea884bca73c52418bdc23a74b3f658aed799
jeffpijeremy <80880882+jeffpijeremy@users.noreply.github.com>
2023-02-10 17:56:48 +0800

    更新送出巡檢填報需要的參數

--- app/src/main/java/com/elliewu/taoyuanapp3/MA3_1_1.kt
+++ app/src/main/java/com/elliewu/taoyuanapp3/MA3_1_1.kt
@@ -288,7 +288,7 @@ fun MA3_1_1(
 //
 //            }
             //Maps_End
-            BottomSpace2(navController,state.lastKnownLocation?.longitude.toString(),state.lastKnownLocation?.latitude.toString());
+            BottomSpace2(navController,WorkTime,WorkCode,state.lastKnownLocation?.longitude.toString(),state.lastKnownLocation?.latitude.toString());
         }
     }
 }
