f8b075f0d8ad5d72e793e394e6b93246f02977ce
mikebimfm <mikebimfm@gmail.com>
2023-02-16 18:09:15 +0800

    capturedImageUri to base64 and set to CurrentPhoto OK

--- app/src/main/java/com/elliewu/taoyuanapp3/CameraTest.kt
+++ app/src/main/java/com/elliewu/taoyuanapp3/CameraTest.kt
@@ -33,7 +33,6 @@ import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
-import coil.compose.ImagePainter.State.Empty.painter
 import coil.compose.rememberImagePainter
 import java.io.ByteArrayOutputStream
 import java.io.File
@@ -42,8 +41,9 @@ import java.text.SimpleDateFormat
 import java.util.*
 import java.util.Base64.getEncoder
 import android.util.Base64
-
-
+import android.provider.MediaStore
+import java.io.FileInputStream
+var CurrentPhoto by mutableStateOf("")
 @Preview
 @Composable
 fun CameraTest(navController: NavHostController = rememberNavController()) {
@@ -99,6 +99,9 @@ fun Camera() {
 
     var capturedImageUri by remember { mutableStateOf(Uri.EMPTY) }
     var cameraCapturedImageUri by remember { mutableStateOf(Uri.EMPTY) }
+
+    var imageBase64 by remember { mutableStateOf<String?>(null) }
+    var fileExistsImageUriPath  by remember { mutableStateOf<String?>(null) }
 
     val cameraLauncher =
         rememberLauncherForActivityResult(ActivityResultContracts.TakePicture()) { success ->
@@ -177,9 +180,39 @@ fun Camera() {
                 contentDescription = null
             )
 
+            Row(
+            ) {
+                Button(modifier = Modifier.padding(horizontal = 8.dp), onClick = {
+
+                    val base64StringOK = imageBase64
+                    val pathOK = fileExistsImageUriPath
+                    if (pathOK != null) {
+                        val fileOK = File(pathOK)
+                        if (fileOK.exists()) {
+                            val base64String = fileToBase64(fileOK)
+                            CurrentPhoto = base64String
+                        }
+                    }
+
+                }) {
+                    Text(text = "Send")
+                }
+            }
+
+            imageBase64 = null
+            fileExistsImageUriPath = null
+            val contextOK: Context = LocalContext.current
+            val uriOK: Uri = capturedImageUri
+            val pathOK = getPathFromUri(contextOK, uriOK)
+            if (pathOK != null) {
+                fileExistsImageUriPath = pathOK
+                val fileOK = File(pathOK)
+                if (fileOK.exists()) {
+                    imageBase64 = fileToBase64(fileOK)
+                }
+            }
+
             if (cameraCapturedImageUri != null) {
-
-
                 val inputStream =
                     LocalContext.current.contentResolver.openInputStream(cameraCapturedImageUri)
                 val MinWenBitmap = BitmapFactory.decodeStream(inputStream)
@@ -195,15 +228,35 @@ fun Camera() {
                 fos.close()
 
                 cameraCapturedImageUri = null
-
-//        val byteArray = byteArrayOf(0x00, 0x01, 0x02, 0x03)
-//        val base64Strina = Base64.encodeToString(byteArray, Base64.DEFAULT)
-//        val base64String = bitmapToBase64(MinWenBitmap)
-
                 Log.i("MyTag", "This is an informational message.")
             }
         }
     }
 }
 
+fun getPathFromUri(context: Context, uri: Uri): String? {
+    val projection = arrayOf(MediaStore.Images.Media.DISPLAY_NAME)
+    val cursor = context.contentResolver.query(uri, projection, null, null, null)
+    if (cursor != null) {
+        val columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DISPLAY_NAME)
+        cursor.moveToFirst()
+        val name = cursor.getString(columnIndex)
+        val file = File(context.cacheDir, name)
+        val inputStream = context.contentResolver.openInputStream(uri)
+        val outputStream = FileOutputStream(file)
+        inputStream?.copyTo(outputStream)
+        inputStream?.close()
+        outputStream.close()
+        cursor.close()
+        return file.absolutePath
+    }
+    return null
+}
 
+fun fileToBase64(file: File): String {
+    val inputStream = FileInputStream(file)
+    val bytes = ByteArray(file.length().toInt())
+    inputStream.read(bytes)
+    inputStream.close()
+    return Base64.encodeToString(bytes, Base64.DEFAULT)
+}
